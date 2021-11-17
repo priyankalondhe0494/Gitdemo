ed3dfb8f08ba0a87bc4db9c77252e245edaf29a6
priyankalondhe0494 <79374474+priyankalondhe0494@users.noreply.github.com>
2021-11-17 17:23:12 +0530

    1st commit

---
+++ Test.java
@@ -0,0 +1,25 @@
+
+public class ifelseassignment 
+{
+	public static void main(String arg[]) 
+	{
+		int x = 99;
+		int y = 98;
+		int z = 102;
+		if(z>x && z>y) 
+		{
+			System.out.println("z is greater than other two");
+		}
+		else if(y>x && y>z)
+		{
+			System.out.println("y is greater than other two");
+			
+		}
+		else  
+		{
+			System.out.println("x is greater than other two");
+		}
+		
+	}
+}
+
